1a8eb3943aa88423653c595f5f8c8d11f1a12c19
U-BSD\418331 <418331@SHS-BYBF6X3.beaverton.k12.or.us>
2024-02-22 12:16:10 -0800

    YAY

--- heap.cpp
+++ heap.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void addNum(int &count, int (&heap)[101]);
 void print(int index, int count, int end, int (heap)[101]);
+void removeNum(int &count, int (&heap)[101]);
 
 int main()
 {
@@ -19,17 +20,35 @@ int main()
   
   while(running == true){
     cout << endl;
-    cout << "Type: ADD, PRINT, DELETE, or QUIT" << endl;
+    cout << "Type: ADD, RANDOM, PRINT, DELETE, or QUIT" << endl;
 
     cin.get(input, 81);
     cin.ignore(81, '\n');
 
+    //cout << heap[count - 1] << endl;
+    
     if (strcmp(input, "ADD") == 0){
       addNum(count, heap);
     }
 
+    else if (strcmp(input, "RANDOM") == 0){
+      int numbers[25];
+      fstream numbers_stream;
+
+      numbers_stream.open("numbers.txt");
+      for (int i = 0; i < 25; i++){
+	int num;
+	numbers_stream.getline
+      }
+      
+    }
+    
     else if (strcmp(input, "PRINT") == 0){
       print(1, 0, count, heap);
+    }
+
+    else if (strcmp(input, "DELETE") == 0){
+      removeNum(count, heap);
     }
     
     else if(strcmp(input, "QUIT") == 0){
@@ -64,6 +83,36 @@ void addNum(int &count, int (&heap)[101]){
     }
   }
   count++;
+}
+
+void removeNum(int &count, int (&heap)[101]){
+  int temp = heap[count - 1];
+  //cout << temp << endl;
+  heap[1] = temp;
+  heap[count - 1] = -1;
+  count--;
+  int currentIndex = 1;
+  while(currentIndex < count){
+    int index1 = currentIndex * 2;
+    int index2 = index1 + 1;
+    int largerIndex;
+    if (heap[index1] < heap[index2]){
+      largerIndex = index2;
+    }
+    else if (heap[index1] > heap[index2]){
+      largerIndex = index1;
+    }
+
+    if(heap[currentIndex] < heap[largerIndex]){
+      int tempNum = heap[currentIndex];
+      heap[currentIndex] = heap[largerIndex];
+      heap[largerIndex] = temp;
+      currentIndex = largerIndex;
+    }
+    else if(heap[currentIndex] >= heap[largerIndex]){
+      currentIndex = count;
+    }
+  }
 }
 
 void print(int index, int count, int end, int (heap)[101]){
